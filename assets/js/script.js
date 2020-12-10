8bc15639aa2b2b5595351fff15c6c08ce423402c
slavabdev <slav.balykov@gmail.com>
2020-12-10 22:35:52 +0000

    add message sent tool to the contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,41 @@
 //Smooth scroll        
-var scroll = new SmoothScroll('a[href*="#"]', {speed: 600});
+let scroll = new SmoothScroll('a[href*="#"]', {speed: 600});
 
 
 //Hiding navbar after clicking
 $('.navbar-collapse a').click(function () {
     $(".navbar-collapse").collapse('hide');
 });
+
+//Send Email
+function sendMail(contactForm) {
+    emailjs.send("service_vd2bbtd","project_request", {
+        'message': contactForm.textmessage.value,
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'subject': contactForm.subject.value
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS', response)
+            alert('YOUR MESSAGE SENT SUCCESSFULY')
+        },
+        function(error) {
+            console.log('FAILED', error)
+        });
+        return false;
+}
+   
+
+//Clean form after submit
+ let form = document.getElementById('contact_form')
+form.addEventListener('submit', function submitForm (event) {
+  event.preventDefault()
+
+  console.table([ ...new FormData(form).entries() ])
+  form.reset()
+})
+
+
 
 
